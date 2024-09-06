c3a88e1a74b44f06ad8b1afd8804703f12c709f5
theEdgeBreaker <thesanjana2002@gmail.com>
2024-09-06 19:47:12 +0530

    Add application files, including CSS and configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "./component/Button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button />
+      </div>
+    </>
+  );
+}
+
+export default App;
